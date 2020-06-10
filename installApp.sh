9bcc87c16f630912e3cdbdae88d24d06025ed36e
dtj2011 <47394965+dtj2011@users.noreply.github.com>
2020-06-10 20:35:24 +0530

    Added url mappings

--- installApp.sh
+++ installApp.sh
@@ -1,2 +1,2 @@
 sudo killall -9 dotnet
-nohup dotnet ./demoapp/build_artifacts/demoapp.dll &>/dev/null &
+nohup dotnet ./demoapp/build_artifacts/demoapp.dll --urls "http://0.0.0.0:5000;https://0.0.0.0:5001" &>/dev/null &
