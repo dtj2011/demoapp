8e0148bd6d85faf644445ecfaaad0155f39cba75
dtj2011 <47394965+dtj2011@users.noreply.github.com>
2020-07-03 12:56:15 +0530

    First commit

--- installApp.sh
+++ installApp.sh
@@ -1,2 +1,2 @@
 sudo killall -9 dotnet
-nohup dotnet /home/ec2-user/demoapp/build_artifacts/demoapp.dll --urls "http://0.0.0.0:5000" 
+nohup dotnet ~/demoapp/build_artifacts/demoapp.dll --urls "http://0.0.0.0:5000"
