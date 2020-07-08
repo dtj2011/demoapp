65927034325d65c7282b94475aa35972e9f58890
dtj2011 <47394965+dtj2011@users.noreply.github.com>
2020-07-08 14:49:22 +0530

    Added powershell commands to move artifacts to IIS website directory

---
+++ moveFiles.ps1
@@ -0,0 +1,4 @@
+$_SourcePath = "C:\Users\Administrator\Desktop\demoapp\build_artifacts"
+$_DestinationPath = "C:\Users\Administrator\Desktop\build_release"
+ 
+Move-item –path $_SourcePath –destination $_DestinationPath
